f02084e0f26dda53ce755cfc396cfc9b61b992c9
Eion Nelson <ecnelson1@live.com>
2021-01-12 15:49:56 -0800

    added app JS to bring function

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 // import functions and grab DOM elements
-const remianGuess = document.getElementById('remain-guess');
+const spanRemianGuess = document.getElementById('remain-guess');
 const userOut = document.getElementById('user-out');
-const Numguess = document.getElementById('user-in');
+const guess = document.getElementById('user-in');
 const subButton = document.getElementById('sub-button'); 
+const secNum = document.getElementById('sec-num');
 
 // initialize state
-const secNum = Math.ceil(Math.random() * 20);
-
+let myNum = Math.ceil(Math.random() * 20);
+let remainGuess = 4;
+spanRemianGuess.textContent = `You Have ${remainGuess} Guesses Remaining!`;
 // set event listeners to update state and DOM
 subButton.addEventListener('click', () => { 
-    console.log(secNum);
-
-})
+    --remainGuess;
+    spanRemianGuess.textContent = `You Have ${remainGuess} Guesses Remaining!`;
+    if (guess.value > myNum){ userOut.textContent = 'TOO High'; } 
+    else if (guess.value < myNum){ userOut.textContent = 'TOO LOW'; } 
+    else { secNum.textContent = `Congratulations! you guessed ${myNum} and were correct!`;
+        guess.disable; }
+    if (remainGuess > 0){
+        spanRemianGuess.textContent = `You Have ${remainGuess} Guesses Remaining!`;
+    }
+    else {userOut.textContent = 'OOPS! YOU ARE OUT OF GUESSES!';
+        guess.disabled = true;}});
+// function compareNumbers(guess, correctNumber){
+//     if (guess > correctNumber){ return 1;}
+//     else if (guess < correctNumber){ return -1;}
+//     else { return 0;}
+// }
