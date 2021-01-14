32428cd39a1e8371bb5ca3d08bb0dc3b8ea04672
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-01-14 15:16:31 -0800

    refactor utils imports to one line

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const spanRemianGuess = document.getElementById('remain-guess');
 const guess = document.getElementById('user-in');
 export const subButton = document.getElementById('sub-button');
 const newGame = document.getElementById('new-game'); 
-import { userOut} from './utils.js';
-import { compareNumbers } from './utils.js';
-import { guessTrack } from './utils.js';
+import { userOut, compareNumbers, guessTrack } from './utils.js';
 // initialize state
 let remainGuess = 4;
 spanRemianGuess.textContent = `You Have ${remainGuess} Guesses Remaining!`;
